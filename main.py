2c24478e7dd10f71c63fdc4a82d97005bfbee571
lanzarglis <Lewinskiy@mail.ru>
2026-04-04 17:31:13 +0500

    Update main.py

--- main.py
+++ main.py
@@ -8,17 +8,17 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Привет! Я Афина — переводчик с кошачьего 🐱\nНапиши мне что-нибудь!")
+    await update.message.reply_text("Привет! Я Мяуфина — переводчик с кошачьего 🐱\nНапиши мне что-нибудь!")
 
 async def translate(update: Update, context: ContextTypes.DEFAULT_TYPE):
     cat_text = update.message.text
-    
+
     translations = {
         "мяу": "привет",
         "мрр": "мурлычу",
         "гав": "пёс",
         "ня": "хочу еды",
-        "фрр": "злая",
+        "ффрр": "злая",
         "хрр": "сплю"
     }
     
@@ -28,16 +28,16 @@ async def translate(update: Update, context: ContextTypes.DEFAULT_TYPE):
 def main():
     token = os.getenv("TELEGRAM_BOT_TOKEN")
     if not token:
-        logger.error("TELEGRAM_BOT_TOKEN не задан!")
+        logger.error("TELEGRAM_BOT_TOKEN не задан")
         return
-    
+
     app = Application.builder().token(token).build()
     
     app.add_handler(CommandHandler("start", start))
     app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, translate))
     
-    logger.info("Бот Афина запущен!")
-    app.run_polling(debug=True)
+    logger.info("Бот Мяуфина запущен!")
+    app.run_polling()
 
 if __name__ == "__main__":
     main()
